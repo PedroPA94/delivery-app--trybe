9129242eef60a12eda8998cff6aff6ad42b4d0a4
rogelins <rogelins@gmail.com>
2023-01-23 15:42:33 -0300

    tasks finished day 01

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const loginRoutes = require('../routes/loginRoutes');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(express.json());
+
+app.use(loginRoutes);
 
 module.exports = app;
---
+++ back-end/src/routes/loginRoutes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+// routes.post('/login', loginController.postLogin);
+
+module.exports = routes;
