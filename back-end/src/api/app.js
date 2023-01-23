02f32a921b2a298a4eb3e22b9af3ce37981fb75f
Pedro Papini <ppapinia@gmail.com>
2023-01-23 14:59:42 -0300

    Adds error middleware on app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const CredentialsValidations = require('../middlewares/CredentialsValidations');
 const ErrorHandler = require('../middlewares/ErrorHandler');
 
 const app = express();
+app.use(express.json())
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.post('/coffee', (_req, res) => res.status(418).end());
 
 app.use(ErrorHandler.handle)
 
