c977390f131f2fdfe333aa4c231b4b319491b34a
rogelins <rogelins@gmail.com>
2023-01-23 17:25:30 -0300

    refactory models files

--- back-end/src/database/models/ProductModel.js
+++ back-end/src/database/models/ProductModel.js
@@ -1,33 +1,67 @@
-import { INTEGER, Model, STRING, DECIMAL } from 'sequelize';
-import db from '.';
-
-class ProductModel extends Model {}
-
-ProductModel.init({
-  id: {
+module.exports = (sequelize, DataTypes) => {
+  const ProductModel = sequelize.define('Product', {
+     id: {
     primaryKey: true,
     autoIncrement: true,
     allowNull: false,
-    type: INTEGER 
+    type: DataTypes.INTEGER 
   },
   name: {
     allowNull: false,
-    type: STRING,
+    type: DataTypes.STRING,
     unique: true
   },
   price: {
     allowNull: false,
-    type: DECIMAL
+    type: DataTypes.DECIMAL
   },
   url_image: {
     allowNull: false,
-    type: STRING,
+    type: DataTypes.STRING,
   },
-}, {
+  }, {
   underscored: true,
   sequelize: db,
   modelName: 'products',
   timestamps: false,
 });
+ProductModel.associate = (models) => {
+  ProductModel.hasMany(models.SaleProductModel, { foreignKey: 'productId', as: 'product'});
+}
 
-export default ProductModel;
+return ProductModel;
+};
+
+// // import { INTEGER, Model, STRING, DECIMAL, DataTypes } from 'sequelize';
+// // import db, { sequelize } from '.';
+
+// class ProductModel extends Model {}
+
+// ProductModel.init({
+//   id: {
+//     primaryKey: true,
+//     autoIncrement: true,
+//     allowNull: false,
+//     type: INTEGER 
+//   },
+//   name: {
+//     allowNull: false,
+//     type: STRING,
+//     unique: true
+//   },
+//   price: {
+//     allowNull: false,
+//     type: DECIMAL
+//   },
+//   url_image: {
+//     allowNull: false,
+//     type: STRING,
+//   },
+// }, {
+//   underscored: true,
+//   sequelize: db,
+//   modelName: 'products',
+//   timestamps: false,
+// });
+
+// export default ProductModel;
