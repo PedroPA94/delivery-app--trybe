197cc8fb2f9594c76902db7d932e6bac770886ca
Pedro Papini <ppapinia@gmail.com>
2023-01-23 14:30:51 -0300

    Implements register input validation

--- back-end/src/middlewares/CredentialsValidations.js
+++ back-end/src/middlewares/CredentialsValidations.js
@@ -2,9 +2,22 @@ class CredentialsValidations {
   static validateLoginInputs(req, _res, next) {
     const { email, password } = req.body;
     if (!email || !password) throw new Error('Invalid empty fields')
-    validateLoginInputs.validateEmail(email);
-    validateLoginInputs.validatePassword(password);
+    CredentialsValidations.validateEmail(email);
+    CredentialsValidations.validatePassword(password);
     next();
+  }
+
+  static validateRegisterInputs(req, _res, next) {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) throw new Error('Invalid empty fields')
+    CredentialsValidations.validateUsername(username);
+    CredentialsValidations.validateEmail(email);
+    CredentialsValidations.validatePassword(password);
+    next();
+  }
+
+  static validateUsername(username) {
+    if (username.length < 12) throw new Error('Username must have at least 12 characters')
   }
 
   static validateEmail(email) {
@@ -13,7 +26,7 @@ class CredentialsValidations {
   }
 
   static validatePassword(password) {
-    if (password.length < 6) throw new Error('Invalid password')
+    if (password.length < 6) throw new Error('Password must have at least 6 characters')
   }
 }
 
