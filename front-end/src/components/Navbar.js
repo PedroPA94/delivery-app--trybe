d7d0ba956a80402c00905a0e23e548387f3b81fa
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-25 16:15:46 -0300

    adding user name in the navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Navbar() {
+  const [user] = useLocalStorage('user');
+
   const navigate = useNavigate();
 
   const handleExit = () => {
@@ -9,53 +12,35 @@ function Navbar() {
   };
 
   return (
-    <div>
-      <div
-        data-testid="customer_products__element-navbar-link-products"
-      >
+    <header>
+      <div data-testid="customer_products__element-navbar-link-products">
         <button
           type="button"
           onClick={ () => navigate('/products') }
         >
           Produtos
         </button>
-
       </div>
-
-      <div
-        data-testid="customer_products__element-navbar-link-orders"
-      >
+      <div data-testid="customer_products__element-navbar-link-orders">
         <button
           type="button"
-          onClick={ () => navigate('/pedidos') }
+          onClick={ () => navigate('/orders') }
         >
           Pedidos
         </button>
       </div>
-
-      <div
-        data-testid="customer_products__element-navbar-user-full-name"
-      >
-        <h1>
-          User
-        </h1>
-
+      <div data-testid="customer_products__element-navbar-user-full-name">
+        <h1>{user.name}</h1>
       </div>
-
-      <div
-        data-testid="customer_products__element-navbar-link-logout"
-
-      >
+      <div data-testid="customer_products__element-navbar-link-logout">
         <button
           type="button"
           onClick={ () => handleExit() }
         >
           Sair
         </button>
-
       </div>
-
-    </div>
+    </header>
   );
 }
 export default Navbar;
