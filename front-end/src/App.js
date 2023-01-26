15ba8ce3b088aa7292dfcdd7d0a348531d511c22
Pedro Papini <ppapinia@gmail.com>
2023-01-26 15:15:25 -0300

    Set redirect to admin after login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
@@ -11,7 +12,7 @@ function App() {
       <Route path="/login" element={ <Login /> } exact />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/admin/manage" element={ <Products /> } />
+      <Route path="/admin/manage" element={ <Admin /> } />
     </Routes>
   );
 }
