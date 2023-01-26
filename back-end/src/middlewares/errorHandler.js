8bb606a42a1d7c6ae27c5ad50d5d61cb3e7149e6
Pedro Papini <ppapinia@gmail.com>
2023-01-26 08:04:32 -0300

    Update error handler to deal with not found image files

--- back-end/src/middlewares/errorHandler.js
+++ back-end/src/middlewares/errorHandler.js
@@ -7,12 +7,14 @@ const errors = [
   { status: 401, message: 'Expired or invalid token' },
   { status: 404, message: 'User not found' },
   { status: 404, message: 'Token not found' },
+  { status: 404, message: 'Image not found' },
   { status: 409, message: 'User already exist' },
 ];
 
 const errorHandler = (error, _req, res, next) => {
+  if (error.code === 'ENOENT') error.message = 'Image not found';
   const errorCode = errors
-    .find((err) => err.message === error.message).status || 500;
+    .find((err) => err.message === error.message)?.status || 500;
   res.status(errorCode).json({ message: error.message });
   next();
 };
