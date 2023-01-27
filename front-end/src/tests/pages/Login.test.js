644c4b364b242c35ccad757e7764f8a2bbcdb496
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-27 17:24:57 -0300

    adding file renderWithRouterAndContext in the Login test

--- front-end/src/tests/pages/Login.test.js
+++ front-end/src/tests/pages/Login.test.js
@@ -1,5 +1,9 @@
+import Provider from '../../AppContext/Provider';
+import Login from '../../pages/Login';
+import renderWithRouterAndContext from '../utils/renderWithRouterAndContext';
+
 describe('Login page', () => {
   it('should have a login form with input elements', () => {
-    const loginPage render
+    const loginPage = renderWithRouterAndContext(<Login />, Provider, ['/login']);
   });
 });
