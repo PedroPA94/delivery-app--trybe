994104772891f881d1f23459678d798f7be27f9e
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-31 16:35:22 -0300

    login integration tests completed

--- back-end/src/tests/integration/login.test.js
+++ back-end/src/tests/integration/login.test.js
@@ -1,55 +1,50 @@
 const sinon = require('sinon');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { User } = require('../../database/models');
 const app = require('../../api/app');
-const md5 = require('md5');
+const loginService = require('../../services/loginService');
+const { loginServiceResult, userFindOne } = require('../mocks/loginMock');
 const { expect } = chai;
 
 chai.use(chaiHttp);
-
-const userInfo = {
-  id: 3,
-  name: 'Cliente Zé Birita',
-  email: 'zebirita@email.com',
-  password: '1c37466c159755ce1fa181bd247cb925',
-  role: 'customer'
-};
 
 describe("Test /login routes", () => {
   describe("Successful answers", () => {
     afterEach(() => sinon.restore());
 
     it("The POST method should return status 200 with a token", async () => {
-      sinon.stub(User, 'findOne').resolves({ dataValues: userInfo});
-      chai.assert.equal('1c37466c159755ce1fa181bd247cb925', md5('$#zebirita#$'));
+      sinon.stub(loginService, 'doLogin').resolves(loginServiceResult);
 
       const response = await chai.request(app).post("/login").send({
         email: "zebirita@email.com",
         password: "$#zebirita#$",
       });
-      console.log(response.error);
+
       expect(response.status).to.be.equal(200);
-      expect(response.body.token).to.exist();
+      expect(response.body.token).to.exist;
     });
   });
 
-  // describe("Client errors", () => {
-  //   beforeEach(async () => {
-  //     sinon.stub(User, "findOne").resolves(null);
-  //   });
+  describe("Client errors", () => {
+    afterEach(() => sinon.restore());
 
-  //   afterEach(() => {
-  //     (User.findOne).restore();
-  //   });
+    it(`shouldn't work if the email field is missing`, async () => {
+      const response = await chai.request(app).post("/login").send({
+        password: "123456",
+      });
 
-  //   it(`shouldn't work if the email field is missing`, async () => {
-  //     const response = await chai.request(app).post("/login").send({
-  //       password: "123456",
-  //     });
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal("Invalid empty fields");
+    });
 
-  //     expect(response.status).to.be.equal(400);
-  //     expect(response.body.message).to.be.equal("All fields must be filled");
-  //   });
-  // });
+    it(`shouldn't work if the email is invalid`, async () => {
+      const response = await chai.request(app).post("/login").send({
+        email: "testemail.com",
+        password: "123456",
+      });
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal("Invalid email");
+    });
+  });
 });
---
+++ back-end/src/tests/mocks/loginMock.js
@@ -0,0 +1,19 @@
+const loginServiceResult = {
+  email: "email@test.com",
+  name: "Customer Example Name",
+  role: "customer",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX"
+};
+
+const userFindOne = {
+  id: 3,
+  name: 'Cliente Zé Birita',
+  email: 'zebirita@email.com',
+  password: '1c37466c159755ce1fa181bd247cb925',
+  role: 'customer'
+};
+
+module.exports = {
+  loginServiceResult,
+  userFindOne,
+};
