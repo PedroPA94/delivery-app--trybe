d6f9f63475329f94ee7630c39696dff6553fa654
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-31 18:50:18 -0300

    adding updates

--- front-end/src/tests/utils/renderWithRouterAndContext.js
+++ front-end/src/tests/utils/renderWithRouterAndContext.js
@@ -1,24 +1,4 @@
 import { render } from '@testing-library/react';
-<<<<<<< HEAD
-import { createMemoryHistory } from 'history';
-import React from 'react';
-import { Router } from 'react-router-dom';
-import Provider from '../../AppContext/Provider';
-
-const renderWithRouterAndContext = (component, AppProvider, historyEntries = ['/']) => {
-  const history = createMemoryHistory({ initialEntries: historyEntries });
-  return {
-    ...render(
-      <Provider value={ AppProvider }>
-        <Router history={ history }>
-          {component}
-        </Router>
-      </Provider>,
-    ),
-    history,
-  };
-};
-=======
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import Provider from '../../AppContext/Provider';
@@ -32,6 +12,5 @@ const renderWithRouterAndContext = (component, AppProvider, historyEntries = ['/
     </Provider>,
   ),
 });
->>>>>>> 7d036658d2f48f2d4e5fbacb4a7f0e3402add467
 
 export default renderWithRouterAndContext;
