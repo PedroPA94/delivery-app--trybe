34f15f4bd0cf418694cc57e1c994a7624dbd0f74
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-31 15:35:48 -0300

    backend tests in progress

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -4,7 +4,7 @@ const { createToken } = require('../utils/createToken');
 
 const doLogin = async (email, loginPassword) => {
   const user = await User.findOne({ where: { email } });
-
+// console.log(user);
   if (!user) throw new Error('User not found');
   if (md5(loginPassword) !== user.password) throw new Error('Incorrect password');
 
--- back-end/src/tests/integration/login.test.js
+++ back-end/src/tests/integration/login.test.js
@@ -1,54 +1,55 @@
-// const sinon = require('sinon');
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const { expect } = chai;
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { User } = require('../../database/models');
+const app = require('../../api/app');
+const md5 = require('md5');
+const { expect } = chai;
 
-// describe("Test /login routes", () => {
-//   describe("Successful answers", () => {
-//     beforeEach(async () => {
-//       sinon.stub(User, "findOne").resolves({
-//         dataValues: {
-//           id: 1,
-//           username: "Admin",
-//           role: "admin",
-//           email: "email@test.com",
-//           password:
-//             "$2a$12$L1YqVHk.DvVw722Ooo48wu6RF//RcWVmfqO20TrxOeoHDynS0qksm",
-//         }
-//       } as User);
-//     });
+chai.use(chaiHttp);
 
-//     afterEach(() => {
-//       (User.findOne as sinon.SinonStub).restore();
-//     });
+const userInfo = {
+  id: 3,
+  name: 'Cliente Zé Birita',
+  email: 'zebirita@email.com',
+  password: '1c37466c159755ce1fa181bd247cb925',
+  role: 'customer'
+};
 
-//     it("The POST method should return status 200 with a token", async () => {
-//       const response = await chai.request(app).post("/login").send({
-//         email: "email@test.com",
-//         password: "123456",
-//       });
+describe("Test /login routes", () => {
+  describe("Successful answers", () => {
+    afterEach(() => sinon.restore());
 
-//       expect(response.status).to.equal(200);
-//       expect(response.body.token).to.exist;
-//     });
-//   });
+    it("The POST method should return status 200 with a token", async () => {
+      sinon.stub(User, 'findOne').resolves({ dataValues: userInfo});
+      chai.assert.equal('1c37466c159755ce1fa181bd247cb925', md5('$#zebirita#$'));
 
-//   describe("Client errors", () => {
-//     beforeEach(async () => {
-//       sinon.stub(User, "findOne").resolves(null);
-//     });
+      const response = await chai.request(app).post("/login").send({
+        email: "zebirita@email.com",
+        password: "$#zebirita#$",
+      });
+      console.log(response.error);
+      expect(response.status).to.be.equal(200);
+      expect(response.body.token).to.exist();
+    });
+  });
 
-//     afterEach(() => {
-//       (User.findOne as sinon.SinonStub).restore();
-//     });
+  // describe("Client errors", () => {
+  //   beforeEach(async () => {
+  //     sinon.stub(User, "findOne").resolves(null);
+  //   });
 
-//     it(`shouldn't work if the email field is missing`, async () => {
-//       const response = await chai.request(app).post("/login").send({
-//         password: "123456",
-//       });
+  //   afterEach(() => {
+  //     (User.findOne).restore();
+  //   });
 
-//       expect(response.status).to.be.equal(400);
-//       expect(response.body.message).to.be.equal("All fields must be filled");
-//     });
-//   });
-// });
+  //   it(`shouldn't work if the email field is missing`, async () => {
+  //     const response = await chai.request(app).post("/login").send({
+  //       password: "123456",
+  //     });
+
+  //     expect(response.status).to.be.equal(400);
+  //     expect(response.body.message).to.be.equal("All fields must be filled");
+  //   });
+  // });
+});
