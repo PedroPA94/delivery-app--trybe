3974881ade24339a6dede848cada2b04486a5fcd
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-02-01 18:57:35 -0300

    starting style components

---
+++ front-end/src/styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const mainColor = '#262624';
+const emphasisColor = 'C99D66';
+const textColor = 'F2F2F2';
+const inputColor = 'FFFFFF';
+const placeholderColor = '949494';
+
+const App = styled.main`
+  background-color: ${mainColor};
+`;
